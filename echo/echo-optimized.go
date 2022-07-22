ac24bd49b5cae2b6ded1f8a27272f34bb4902b78
Muluneh Derese <muluneh.derese@protonmail.com>
2022-07-22 10:47:29 +0300

    echo linux program

---
+++ echo/echo-optimized.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(time.Since(start).Seconds())
+}
